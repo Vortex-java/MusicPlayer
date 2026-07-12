3172c10abb87660ae5dd4e3228c55ff65c81b986
Vortex-java <daniilpasenko760@gmail.com>
2026-07-12 18:22:39 +0300

    Инициализация проекта

---
+++ src/test/java/app/vx/musicplayer/MusicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vx.musicplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
