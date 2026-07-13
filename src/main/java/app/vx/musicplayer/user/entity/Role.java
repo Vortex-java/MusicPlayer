03656f330cc429a953d2fef59e876dc65d5c1d70
Vortex-java <daniilpasenko760@gmail.com>
2026-07-13 13:57:54 +0300

    Добавлены User и Role

---
+++ src/main/java/app/vx/musicplayer/user/entity/Role.java
@@ -0,0 +1,6 @@
+package app.vx.musicplayer.user.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/app/vx/musicplayer/user/entity/User.java
@@ -0,0 +1,29 @@
+package app.vx.musicplayer.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
